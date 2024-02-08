9d19780ffbd9dd0f207c97f54841e8f3ae9b805d
Victor Mello <victormello7003@hotmail.com>
2024-02-08 15:07:01 -0300

    Configurando prisma orm

--- src/http/server.ts
+++ src/http/server.ts
@@ -6,7 +6,6 @@ app.get('/hello', () => {
   return 'Hello World'
 })
 
-app.listen({port : 3333}).then(() => {
+app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333');
-  
 })
