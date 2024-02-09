d8232601ed4e594ac38aef915a83b90dd0594afd
Victor Mello <victormello7003@hotmail.com>
2024-02-09 15:55:44 -0300

    Criando rota para votos

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 import { createPoll } from './Routes/create-poll'
 import { getPoll } from './Routes/get-poll'
+import { voteOnPoll } from './Routes/vote-on-poll'
 
 const app = fastify()
 
+app.register(cookie, {
+  secret: 'polls-app-nlw',
+  hook: 'onRequest',
+})
+
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on port 3333');
